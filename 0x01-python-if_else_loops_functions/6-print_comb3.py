935bc5e5fe1cbc78f0eacfc8f451edc29b2f7970
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-03 00:51:30 +0300

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(100):
+    if (i / 10 > i % 10):
+        continue
+    elif (i == 89):
+        print("{}".format(i))
+    else:
+        print("{:02}, ".format(i), end='')
