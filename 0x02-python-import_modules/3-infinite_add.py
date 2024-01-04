8c33759de492b01b9e184fd94c019ce915c49436
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-04 14:57:05 +0300

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 sum = 0
-for i in range(1, len(sys.argv)):
-    sum = int(sys.argv[i]) + sum
+for i in range (1, len(sys.argv)):
+    sum = sum + int(sys.argv[i])
 print("{}".format(sum))
 
