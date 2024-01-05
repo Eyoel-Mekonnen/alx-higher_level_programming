ec5262c8a04c5e3a3667501da003bfa8a10852bd
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-05 11:19:13 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if (len(sys.argv) == 1):
         print("{} arguments.".format(0))
     if (len(sys.argv) == 2):
-        print("{} argument:".format(len(sys.argv) -1))
+        print("{} argument:".format(len(sys.argv) - 1))
     elif (len(sys.argv) > 2):
         print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(len(sys.argv)):
