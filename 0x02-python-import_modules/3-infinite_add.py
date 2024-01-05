92ba680180c25b69adc70139dc4454dde058b5f2
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-05 11:17:38 +0300

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
 
     sum = 0
 
-    for i in range (1, len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         sum = sum + int(sys.argv[i])
     print("{}".format(sum))
-
