40b58e9409f4e99679f48db6020080b787e7fbf6
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-09 15:50:09 +0300

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     list_2 = []
     for i in range(0, len(my_list)):
         if (my_list[i] % 2 == 0):
-            list_2.append("True")
+            list_2.append(True)
         elif (my_list[i] % 2 != 0):
-            list_2.append("False")
+            list_2.append(False)
     return list_2
