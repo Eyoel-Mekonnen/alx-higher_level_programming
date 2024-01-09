384fa5fde2dd186443a383623970e4b2c94950f3
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-09 12:47:47 +0300

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = []
+    if not isinstance(tuple_a, tuple):
+        tuple_a = (tuple_a,)
+    if not isinstance(tuple_b, tuple):
+        tuple_b = (tuple_b,)
     len_a = len(tuple_a)
     len_b = len(tuple_b)
     if (len_a == 0 and len_b == 0):
-        new_tuple = [0,0]
+        new_tuple = [0, 0]
         new_tuple = tuple(new_tuple)
         return new_tuple
     for i in range(0, 2):
@@ -13,6 +18,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         elif (len_b > len_a and i >= len_a):
             new_tuple.append(tuple_b[i])
         else:
-            new_tuple.append(tuple_a[i] + tuple_b[i])
+            if (i < len_a and i < len_b):
+                new_tuple.append(tuple_a[i] + tuple_b[i])
+            else:
+                new_tuple.append(0)
+
     new_tuple = tuple(new_tuple)
     return new_tuple
--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -5,8 +5,8 @@ add_tuple = __import__('7-add_tuple').add_tuple
 #tuple_b = (88, 11)
 #tuple_a = (1, 2,3)  
 #tuple_b = (1, 2)
-tuple_a = ()
-tuple_b = ()
+tuple_a = (1)
+tuple_b = (1)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
