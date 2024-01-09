5a7fde618a6c01005fd667e566694ade40450525
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-09 11:49:54 +0300

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,13 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = []
     len_a = len(tuple_a)
     len_b = len(tuple_b)
-    if len_a > len_b:
-        length = len_a
-    elif len_a == len_b:
-        length = len_a
-    else:
-        length = len_b
-    for i in range(0, length):
+    for i in range(0, 2):
         if (len_a > len_b and i >= len_b):
             new_tuple.append(tuple_a[i])
         elif (len_b > len_a and i >= len_a):
