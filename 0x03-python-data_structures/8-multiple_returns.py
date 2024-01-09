d853ab192c36264cd025e11e05b576df8dedd4bc
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-09 15:00:01 +0300

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if (sentence == '' or sentence is None):
+        return None
+    tuple_list = []
+    tuple_list.append(len(sentence))
+    tuple_list.append(sentence[0])
+    tuple_list = tuple(tuple_list)
+    return tuple_list
