fe4d060173d7c4afecb3d010e4ca6ace40631353
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-10 23:12:32 +0300

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        value = a_dictionary[key]
-        a_dictionary[key] = value * 2
-    return a_dictionary
+    new_dictionary = a_dictionary.copy()
+    for key in new_dictionary:
+        value = new_dictionary[key]
+        new_dictionary[key] = value * 2
+    return new_dictionary
 
