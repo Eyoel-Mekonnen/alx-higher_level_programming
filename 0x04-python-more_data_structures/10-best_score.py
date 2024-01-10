dba3ea140bed388d60cbb4badc4a7729bc1fef9b
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-10 23:37:37 +0300

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    sort_value = []
+    if a_dictionary is None:
+        return None
+    for key in a_dictionary:
+        sort_value.append(a_dictionary[key])
+    sort_value.sort()
+    max_value = sort_value[len(sort_value) - 1]
+    for key in a_dictionary:
+        if (a_dictionary[key] == max_value):
+            return key
