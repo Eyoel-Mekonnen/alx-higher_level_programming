edf2bc65213eab5b8a518d9c5d20f12851477b28
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-23 23:50:35 +0300

    task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class square"""
+
+
+class Square:
+    """Instanization of size."""
+
+    def __init__(self, size):
+        """"creating a prviate attribute."""
+
+        self.__size = size
