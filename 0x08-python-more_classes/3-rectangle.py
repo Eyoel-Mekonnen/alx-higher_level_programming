772b3d4bcb9d79eaba039b97e4c4e1da6c87f174
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-30 15:29:52 +0300

    task

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,6 +51,8 @@ class Rectangle:
         return (2*(self.__width) + 2*(self.__height))
 
     def __str__(self):
+        if (self.__width == 0 or self.__height == 0):
+            return ("")
         string = ""
         for i in range(0, self.__height):
             for j in range(0, self.__width):
