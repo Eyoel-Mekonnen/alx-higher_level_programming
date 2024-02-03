e436a2b64709c2a78e3653520cfa82b446a885e8
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-03 21:19:26 +0300

    task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,7 +3,41 @@
 
 
 def matrix_divided(matrix, div):
+    """ Divide all elements of a matrix.
+
+    Args:
+        matrix -it takes a list of list that contain float or integers
+        div - a number to divide each element of the list
+    Raises:
+        TypeError: if matrix is not a list
+        TypeError: if matrix is not a list of list
+        TypeError: if row of the matirx is not consitent
+        TypeError: if memebers of row are not integers or floats
+        ZeroDivisionError: if the div argument is zero
+    Returns:
+        A new matrix that divides each element by div
+    """
+
     matrix_new = []
+
+    length = len(matrix[0])
+    if not (isinstance(matrix, list)):
+        raise TypeError("matrix must be a matrix (list of lists) of"
+                        "integers/floats")
+    for i in matrix:
+        if not (isinstance(i, list)):
+            raise TypeError("matrix must be a matrix (list of lists) of"
+                            "integers/floats")
+        if len(i) != length:
+            raise TypeError("Each row of the matrix must have the same size")
+    for member in i:
+        if not (isinstance(member, (int, float))):
+            raise TypeError("matrix must be a matrix (list of lists) of "
+                            "integers/floats")
+    if not (isinstance(div, (float, int))):
+        raise TypeError("div must be a number")
+    if (div == 0):
+        raise ZeroDivisionError("division by zero")
     for i in range(0, len(matrix)):
         row_matrix = []
         for j in range(0, len(matrix[i])):
