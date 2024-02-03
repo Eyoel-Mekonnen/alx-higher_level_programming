990a0aac64c78c5f3b7236d5c29934f564c31151
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-04 01:37:23 +0300

    task

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Method that pring #"""
+
+
+def print_square(size):
+    """print square.
+        Args: size
+        Raise: size is not an integer
+               size must not be less than 0
+               size is float and less than 0
+        Return:
+            prints the #
+    """
+    if not (isinstance(size, int)):
+        raise TypeError("size must be an integer")
+    if (size < 0):
+        raise ValueError("size must be >= 0")
+    if (isinstance(size, float) and size < 0):
+        raise TypeError("size must be an integer")
+    for i in range(0, size):
+        for j in range(0, size):
+            print("#", end="")
+        print("")
