6e99668384a86b1a329a3158dd70ee4843de80a6
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-04 13:44:51 +0300

    task

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Prints a text with two new lines"""
+
+
+def text_indentation(text):
+    """Text indentation method
+        Args: text
+        Raise: text must be a string
+        Return: prints text with two lines after characters ., ? and :
+    """
+    if not (isinstance(text, str)):
+        raise TypeError("text must be a string")
+    for i in range(0, len(text)):
+        if (i > 0):
+            if (text[i - 1] == "." or text[i - 1] == "?" or text[i - 1] == ":"):
+                print("")
+                print("")
+            else:
+                print("{}".format(text[i]), end="")
+        elif (i < len(text) - 1):
+            print("{}".format(text[i]), end="")
