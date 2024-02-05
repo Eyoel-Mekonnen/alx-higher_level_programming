9defdb9b5eef30c080b1d256de31229115be4475
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-05 13:04:58 +0300

    task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+"""Returns list of avalialbe attributes and methods"""
+
+
+def lookup(obj):
+    """method will use dic() to list the attributes and methods"""
+
+    return (dir(obj))
