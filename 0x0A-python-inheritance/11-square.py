959cba60718ec55cf5502f11c7e02dec52ae3916
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-05 21:52:57 +0300

    task

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,5 +9,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
     def __str__(self):
+        """Polymorphism of str function """
         return ("[Square] {}/{}".format(self.__size, self.__size))
