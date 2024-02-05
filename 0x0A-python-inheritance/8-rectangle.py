4d95b5b550edcdbfe79192ea0bdef85de9c856a1
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-05 18:09:26 +0300

    task

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """Class Rectangle """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 def Rectangle(BaseGeometry):
     """instantiaiton will happen"""
 
     def __init__(self, width, height):
-        integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
