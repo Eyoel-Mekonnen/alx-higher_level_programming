32e3f66d2e2c012caab886eb09540aa774a1460b
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-05 17:12:33 +0300

    task

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ def inherits_from(obj, a_class):
     """checking if it is inherited directly
         or indirectly as well as is an instance the
             type checking insures it is inhertited"""
-    if (isinstance(obj, a_class) and type(obj) is a_class):
+    if (isinstance(obj, a_class) and type(obj) is not a_class):
         return (True)
     else:
         return (False)
