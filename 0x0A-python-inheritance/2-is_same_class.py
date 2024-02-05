26a4537d6d5d5470b6a3eb47d2d8a3b13f766776
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-05 16:16:59 +0300

    task

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Method to check wether it is an instance of a class or not"""
+
+
+def is_same_class(obj, a_class):
+    """Method will now check if it is an instance or not"""
+    return (isinstance(obj, a_class))
