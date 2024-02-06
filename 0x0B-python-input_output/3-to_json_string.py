d5967ceb497f6255b0c6fa5a4e82a0a88039bce1
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-06 22:02:14 +0300

    task

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+import json
 """Method that will return JSON objects String"""
 
 
-def to_jason_string(my_obj):
+def to_json_string(my_obj):
     """Defining method to convert to JASON"""
     jason_data = json.dumps(my_obj)
     return (jason_data)
