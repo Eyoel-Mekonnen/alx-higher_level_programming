01d1d94a4474ee90083682f36272601e5145815f
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-06 21:41:34 +0300

    task

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Method that will append"""
+
+
+def append_write(filename="", text=""):
+    """Defining an append method"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        append_data = f.write(filename)
+    return (append_data)
