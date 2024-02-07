a00cc1ffefa3d2757f3626ffccb9d9f12ce91d53
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-07 13:23:04 -0800

    task

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,8 +6,8 @@ class Student:
     """Class student instiation."""
     def __init__(self, first_name, last_name, age):
         """instance of student"""
-        self.first_name = name
-        self.last_name = name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
