9ce892196ae7eaf81be8951aa497becbea65a4aa
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-11 13:12:57 +0300

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@
 class Base:
     """Base class defined."""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """Base class initialized."""
         if (id is not None):
@@ -12,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of dict."""
+        if list_dictionaries is None or list_dictionaries == []:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
