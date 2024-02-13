dd7ab06036f1dc433b58960dc47562457fe61f64
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-13 16:56:30 +0300

    task

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -12,3 +12,30 @@ class Test_Rectangle(unittest.TestCase):
         self.assertIsInstance(rectangle, Base)
         self.assertEqual(10, rectangle.width)
         self.assertEqual(2, rectangle.height)
+        rectangle2 = Rectangle(1, 2, 3)
+        self.assertEqual(1, rectangle2.width)
+        self.assertEqual(2, rectangle2.height)
+        self.assertEqual(3, rectangle2.x)
+        rectangle3 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(1, rectangle3.width)
+        self.assertEqual(2, rectangle3.height)
+        self.assertEqual(3, rectangle3.x)
+        self.assertEqual(4, rectangle3.y)
+    
+    def test_if_parameters_are_correct(self):
+        """Checks wether or not passed parameters are integers."""
+        with self.assertRaises(TypeError, msg="width must be an integer"):
+            Rectangle("1", 2)
+        with self.assertRaises(TypeError, msg="height must be an integer"):
+            Rectangle(1, "2")
+        with self.assertRaises(TypeError, msg="x must be an integer"):
+            Rectangle(1, 2, "3")
+        with self.assertRaises(TypeError, msg="y must be an integer"):
+            Rectangle(1, 2, 3, "4")
+        rectangle = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(1, rectangle.width)
+        self.assertEqual(2, rectangle.height)
+        self.assertEqual(3, rectangle.x)
+        self.assertEqual(4, rectangle.y)
+        self.assertEqual(5, rectangle.id)
+
