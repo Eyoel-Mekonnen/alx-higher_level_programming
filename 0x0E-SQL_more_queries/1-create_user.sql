c1e382352dd42d53421a1f5d59f937ea1404ca33
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-13 21:41:42 +0300

    task

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,20 +1,4 @@
--- Check if the user already exists
-SELECT COUNT(*) INTO @user_exists
-FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost';
-
--- Create the user if it does not exist
-SET @create_user_query = IF(@user_exists = 0, 
-                            'CREATE USER \'user_0d_1\'@\'localhost\' IDENTIFIED BY \'user_0d_1_pwd\';', 
-                            'SELECT \'User already exists\' status;');
-PREPARE stmt FROM @create_user_query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
-
--- Grant privileges only if the user was just created
-SET @grant_privileges_query = IF(@user_exists = 0,
-                                 'GRANT ALL PRIVILEGES ON *.* TO \'user_0d_1\'@\'localhost\'; FLUSH PRIVILEGES;', 
-                                 'SELECT \'User already had privileges\' status;');
-PREPARE stmt FROM @grant_privileges_query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+-- Check if the user already exists and  create user_0d_1
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
 
