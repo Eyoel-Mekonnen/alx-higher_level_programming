b51fd6c3d2e905d94c3af1052032e901a388d917
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-14 15:01:20 +0300

    task

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,5 @@
 -- create a table called force_name
 CREATE TABLE IF NOT EXISTS force_name(
-	id INT AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+	id INT,
+	name VARCHAR(256) NOT NULL
 );
