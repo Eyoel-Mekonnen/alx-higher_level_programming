caf07a394da4270d65aceff06123d9b58cd7758c
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-14 13:11:26 +0300

    taskhotfix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,8 @@
 -- script that creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- script that creates a new user with password
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'usr_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- script that grants select privilige for the user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+-- grant the privilges and make them appy
+FLUSH PRIVILEGES;
