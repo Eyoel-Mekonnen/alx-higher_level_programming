a5c22d5c92e4932b0fa0fbbbe0a5044784141807
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-14 17:30:49 +0300

    task_7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- create a Database htbn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use the created database
+USE hbtn_0d_usa
+-- create a table that will reference to previous table
+CREATE TABLE IF NOT EXISTS cities(
+	id INT AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	PRIMARY KEY (id),
+	FORIEGN KEY (state_id) REFERENCES (states)
+);
