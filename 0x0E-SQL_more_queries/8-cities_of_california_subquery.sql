807507833e7462511ef42f6a9e057aa15f761874
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-14 20:47:31 +0300

    task_8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- lists all ciites of california
 USE hbtn_0d_usa
-SELECT  *
-FROM hbtn_0d_usa.states
-WHERE name = "California"
-ORDER BY id ASC;
+SELECT id, name
+FROM cities
+WHERE cities.state_id = (SELECT states.id FROM states.name = 'California')
+ORDER BY cities.id ASC;
