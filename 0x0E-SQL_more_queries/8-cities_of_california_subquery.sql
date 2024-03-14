bc260cd274542cf9e486a339d9e894dafcd1dbd0
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-14 20:49:20 +0300

    task_8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 USE hbtn_0d_usa
 SELECT id, name
 FROM cities
-WHERE cities.state_id = (SELECT states.id FROM states.name = 'California')
+WHERE cities.state_id = (SELECT states.id FROM states WHERE states.name = 'California')
 ORDER BY cities.id ASC;
