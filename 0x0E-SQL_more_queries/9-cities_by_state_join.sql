a6664ca6e991abe1a6844a8f93365b2f2c67f969
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-15 11:01:32 +0300

    task_9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- script that list all cities contained in the dtatabase
 USE hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM cities, states
-LEFT JOIN states ON cities.state_id = state.id
-ORDER BY cities.id ASC;
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC
