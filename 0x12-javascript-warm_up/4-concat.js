0f262e62177cba1d2a71f061f8209bedeb5b3778
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-09 20:18:33 +0300

    task4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,16 +1,4 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (argv[2] === undefined) {
-  process.stdout.write('undefined');
-} else if (argv[2] !== undefined) {
-  process.stdout.write(argv[2]);
-}
-process.stdout.write(' is ');
-if (argv[3] !== undefined) {
-  process.stdout.write(argv[3]);
-  process.stdout.write('\n');
-} else if (argv[3] === undefined) {
-  process.stdout.write('undefined');
-  process.stdout.write('\n');
-}
+console.log(`${argv[2]} ${argv[3]}`);
