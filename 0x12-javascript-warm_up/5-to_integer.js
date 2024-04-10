7460ce7d4a5462a3ad93c3ead2c212c69486ae9a
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-10 17:21:30 +0300

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const num = parseInt(argv[2], 10);
-if (typeof (num) !== 'number' || typeof (argv[2]) === 'string') {
+
+if (isNaN(num)) {
   console.log('Not a number');
 } else {
   console.log(`My number: ${num}`);
