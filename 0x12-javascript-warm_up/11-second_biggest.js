e84cee51ff38f652acb602c34396b89515ea32eb
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-11 10:05:51 +0300

    task11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const { argv } = require('process');
+if (argv.length === 2) {
+  console.log('0');
+}
+const array = [];
+for (let i = 2; i < argv.length; i++) {
+  let value = parseInt(argv[i]);
+  if (argv.length === 3 && !isNaN(value)) {
+    value = value - 1;
+    console.log(value);
+  } else if (!isNaN(value)) {
+    array.push(value);
+  }
+}
+if (array.length !== 0) {
+  array.sort();
+  console.log(array[array.length - 2]);
+}
