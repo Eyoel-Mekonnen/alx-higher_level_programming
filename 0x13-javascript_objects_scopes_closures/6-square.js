0f4b5a4569776a37380ad533b1ef5c4bc1a5dbd1
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-12 14:56:49 +0300

    task6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
-const Square_custom = require('./5-square');
-class Square extends Square_custom {
+const SquareCustom = require('./5-square');
+class Square extends SquareCustom {
   constructor (size) {
     super(size);
     this.size = size;
   }
+
   charPrint (c) {
     if (c === undefined) {
-      this.print ();
-    }
-    else {
+      this.print();
+    } else {
       for (let i = 0; i < this.size; i++) {
         let array = '';
         for (let j = 0; j < this.size; j++) {
           array = array + c;
-	}
-	console.log(array);
+        }
+        console.log(array);
       }
     }
   }
