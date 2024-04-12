63702ec62be66779c1d0d6cfc4e93d3d18e3ec82
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-12 13:00:51 +0300

    task4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -18,11 +18,11 @@ class Rectangle {
   }
 
   rotate () {
-    let tmp = this.width;
+    const tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
-  
+
   double () {
     this.width = 2 * this.width;
     this.height = 2 * this.height;
