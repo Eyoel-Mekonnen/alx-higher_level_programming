c245ba636a96e70796c7f04a06e8aa3bcf99d535
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-12 12:57:31 +0300

    task4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let array = '';
+      for (let j = 0; j < this.width; j++) {
+        array = array + 'X';
+      }
+      console.log(array);
+    }
+  }
+
+  rotate () {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+  
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.width;
+  }
+}
+module.exports = Rectangle;
