a5515719d7ed0542a001def8d5e39500a9eb021d
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-13 18:03:39 +0300

    task13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all genres from hbtn_0d_tvshows and display the number of linked to each
 
-SELECT tv_genre.name as genre, COUNT(tv_show_genre.genre_id) as 'number_of_shows'
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres
 RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
