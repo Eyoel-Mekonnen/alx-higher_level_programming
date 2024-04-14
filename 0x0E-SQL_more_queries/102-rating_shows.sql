9c418fe5144e242375304232d9b695fdb6677cf1
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-14 14:18:51 +0300

    task102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,6 +2,6 @@
 
 SELECT tv_shows.title, SUM(tv_shows_rating.rate) AS 'rating'
 FROM tv_shows
-JOIN tv_show_rating ON tv_show_rating.show_id = tv_shows.id
+JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
