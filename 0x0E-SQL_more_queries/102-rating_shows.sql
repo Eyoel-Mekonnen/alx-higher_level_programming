af4f5b18f946fbf0040d4cf57bee6bf647cf01f2
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-14 14:19:42 +0300

    task102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows by their rating
 
-SELECT tv_shows.title, SUM(tv_shows_rating.rate) AS 'rating'
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
 FROM tv_shows
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
