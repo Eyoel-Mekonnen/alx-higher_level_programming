365e81b10ac64f9d6bf099ff045b4bb9a537a3f9
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-14 13:39:13 +0300

    task100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,8 +1,10 @@
 -- lists those that are not Dexter
 SELECT tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_shows.title != 'Dexter'
+FROM tv_genres
+WHERE tv_genres.id NOT IN (
+	SELECT tv_show_genres.genre_id
+	FROM tv_show_genres
+	JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_shows.title = 'Dexter'
+)
 ORDER BY tv_genres.name ASC;
-
