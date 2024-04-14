82d313bc4063529ab86d0a6f99561005d05b0b60
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-14 10:20:05 +0300

    task14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,4 @@ FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_genres.name ASC;
