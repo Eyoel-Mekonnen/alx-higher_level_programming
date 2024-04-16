c6f707932ea1dbcac6aaa732af88386adde44e16
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-16 15:06:40 +0300

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""scripts takes 4 arguments and display values"""
+
+import MySQLdb
+import sys
+
+
+def display(username, password_, db_name, state):
+    """searches for the required state"""
+    db = MySQLdb.connect(host="localhost", user=username,
+                         database=db_name, port=3306)
+    cursor = db.cursor()
+    cursor.execute("""SELECT states.id, states.name FROM
+                   states WHERE states.name = %s""", (state,))
+    results = cursor.fetchall()
+    for rows in results:
+        print(rows)
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) == 5):
+        display(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    else:
+        print("Usage: python script.py <username> <password> <dbname>")
