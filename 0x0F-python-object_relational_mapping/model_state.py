8236cf079fdd7d7baa432e6cd35c1ad0837d2020
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-17 13:08:27 +0300

    task6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""File that contains class Defination of state."""
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State Class Defined here."""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 4):
+        print("Numbe of argument is small")
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
