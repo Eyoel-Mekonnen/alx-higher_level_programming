1d126723c6f57c864c390055726f32383c8b86d1
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-30 12:09:52 +0300

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""script that fetches website"""
+
+"""
+script that fetches website.
+
+fetches https://alx-intranet.hbtn.io/status.
+"""
+
 
 if __name__ == '__main__':
-    import urllib import request
+    from urllib import request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         data = r.read()
-        print("Body response: $")
+        print("Body response:")
         print("\t- type : {}".format(type(data)))
         print("\t- content: {}".format(data))
         print("\t- utf8 content: {}".format(data.decode("utf-8")))
