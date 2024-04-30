30573571ad42f5ce92eaae9420d773fad464ac1c
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-30 12:11:56 +0300

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,11 +8,11 @@ fetches https://alx-intranet.hbtn.io/status.
 
 
 if __name__ == '__main__':
-    from urllib import request
+    import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         data = r.read()
         print("Body response:")
-        print("\t- type : {}".format(type(data)))
+        print("\t- type: {}".format(type(data)))
         print("\t- content: {}".format(data))
         print("\t- utf8 content: {}".format(data.decode("utf-8")))
