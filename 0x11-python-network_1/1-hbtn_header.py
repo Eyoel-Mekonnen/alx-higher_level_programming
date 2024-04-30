2e92a67d1bcc15430205ff148e061b73837d636d
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-30 12:31:23 +0300

    task1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Display the value of X-Request-ID"""
+import sys
+
+if __name__ == '__main__':
+    import urllib.request
+my_request = sys.argv[1]
+with urllib.request.urlopen(my_request) as r:
+    id_request = r.getheader('X-Request-ID')
+    print(id_request)
