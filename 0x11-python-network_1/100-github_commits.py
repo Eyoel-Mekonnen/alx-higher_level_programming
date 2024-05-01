daf6aa3c89d50ac8dfcc864460f852b54428e520
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-05-01 20:33:30 +0300

    task100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,13 +7,15 @@ import requests
 if __name__ == '__main__':
     repository = sys.argv[1]
     owner = sys.argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repository)
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner,
+                                                              repository)
     response = requests.get(url)
     if response.status_code == 200:
         json_ = response.json()
         count = 0
         for commits in json_:
-            print("{}: {}".format(commits['sha'], commits['commit']['author']['name']))
+            print("{}: {}".format(commits['sha'],
+                  commits['commit']['author']['name']))
             count = count + 1
             if count >= 10:
                 break
