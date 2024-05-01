831b5acba4df2603a0409393a7c888bb2d451af1
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-05-01 20:47:45 +0300

    task101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Send JSON POST request to a URL
-FILE=$2
-URL=$1
-curl -X POST -H "Content-Type: application/json" -d @"$FILE" "$URL"
+curl -X POST -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
 
