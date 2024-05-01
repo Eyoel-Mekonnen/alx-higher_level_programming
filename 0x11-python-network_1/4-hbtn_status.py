a45b7d90107902ee3df70175c736a0932a3a7f0d
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-05-01 03:13:10 +0300

    task4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Use request to request"""
+"""Use request to request."""
 import requests
 
 if __name__ == '__main__':
     response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(response.txt)))
-    print("\t- content: {}".format(response.txt))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
