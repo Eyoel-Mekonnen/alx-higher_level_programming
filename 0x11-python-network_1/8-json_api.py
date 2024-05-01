710e4797273ac345357bc33278ea7e79e6073a22
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-05-01 14:31:06 +0300

    task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,11 +5,11 @@ import sys
 
 if __name__ == '__main__':
     if (len(sys.argv) == 2):
-        variable = sys.argv[1]
+        q = sys.argv[1]
     else:
-        variable = ""
-    q = {'q': variable}
-    response = requests.post("http://0.0.0.0:5000/search_user", data=q)
+        q = ""
+    data = {'q': q}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
     json_ = response.json()
     if json_ == {}:
         print("No result")
