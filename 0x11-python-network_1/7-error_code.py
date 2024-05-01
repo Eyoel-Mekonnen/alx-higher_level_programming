cf0d6f78801de432d21fba5a548dd3b85bca189d
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-05-01 13:02:45 +0300

    task7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    response = request.post(url)
+    response = requests.post(url)
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
